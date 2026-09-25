8446ce629a9b4c626e71cd27da7f0ad526e86036
menchaca-andres <4mrvro@gmail.com>
2026-09-25 17:20:22 -0400

    Initial Commit

---
+++ reecicla-app/reecicla_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  reecicla_appApp.swift
+//  reecicla-app
+//
+//  Created by Andrés on 25/9/26.
+//
+
+import SwiftUI
+
+@main
+struct reecicla_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ reecicla-appTests/reecicla_appTests.swift
@@ -0,0 +1,19 @@
+//
+//  reecicla_appTests.swift
+//  reecicla-appTests
+//
+//  Created by Andrés on 25/9/26.
+//
+
+import Testing
+@testable import reecicla_app
+
+struct reecicla_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
